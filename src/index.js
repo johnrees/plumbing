7f96542b417a42f856187da8ada934ab4685eaca
John Rees <john@bitsushi.com>
2016-09-21 12:31:20 +0100

    es6 - jsplumb & webpack fully working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import jsPlumb from 'jsPlumb'
 
-jsPlumb.on(window, "resize", jsPlumb.repaintEverything);
+jsPlumb.ready(() => {
+
+  jsPlumb.getInstance({
+    Connector: "Flowchart",
+    Anchor: "Bottom",
+    Endpoint: [ "Dot", { radius: 2 }],
+    ConnectionOverlays: [
+      [ "Arrow", { location: 0.5, width: 10, length: 7, foldbackPoint: 1, direction: 1 }]
+    ]
+  });
+
+  jsPlumb.connect({source: "one", target: "two"});
+
+  jsPlumb.on(window, "resize", jsPlumb.repaintEverything);
+
+});
